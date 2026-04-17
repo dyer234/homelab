67aace024b6612299581ae3dd6e2e38c4b6b1131
Daniel Dyer <dyer234@gmail.com>
2026-04-17 08:14:17 -0700

    add gluetun

--- scripts/up.sh
+++ scripts/up.sh
@@ -9,5 +9,8 @@ for app in $(get_apps); do
   if [ -e /dev/dri ] && [ -f "$HOMELAB_DIR/$app/docker-compose.gpu.yml" ]; then
     compose_files="$compose_files -f docker-compose.gpu.yml"
   fi
-  (cd "$HOMELAB_DIR/$app" && docker compose $compose_files up -d)
+  if [ "${NO_ACME:-}" = "true" ] && [ -f "$HOMELAB_DIR/$app/docker-compose.no-acme.yml" ]; then
+    compose_files="$compose_files -f docker-compose.no-acme.yml"
+  fi
+  (cd "$HOMELAB_DIR/$app" && docker compose $compose_files up -d --remove-orphans)
 done
