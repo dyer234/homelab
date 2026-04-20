604b3035b7efe97fbccaeb2c45a1705a6db44be5
Daniel Dyer <dyer234@gmail.com>
2026-04-19 21:12:36 -0700

    add tun for gluetun

--- scripts/up.sh
+++ scripts/up.sh
@@ -6,8 +6,8 @@ source "$(dirname "$0")/defines.sh"
 for app in $(get_apps); do
   echo "Starting $app..."
   compose_files="-f docker-compose.yml"
-  if [ -e /dev/dri ] && [ -f "$HOMELAB_DIR/$app/docker-compose.gpu.yml" ]; then
-    compose_files="$compose_files -f docker-compose.gpu.yml"
+  if [ "$(uname)" = "Linux" ] && [ -f "$HOMELAB_DIR/$app/docker-compose.linux.yml" ]; then
+    compose_files="$compose_files -f docker-compose.linux.yml"
   fi
   if [ "${NO_ACME:-}" = "true" ] && [ -f "$HOMELAB_DIR/$app/docker-compose.no-acme.yml" ]; then
     compose_files="$compose_files -f docker-compose.no-acme.yml"
